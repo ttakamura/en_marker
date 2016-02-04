e2377a02bdf1fe596d965ff0ca827b06ea658519
Tatsuya Takamura <takamura@tonchidot.com>
2016-02-05 06:44:26 +0900

    [from now] Fri Feb  5 06:44:26 JST 2016

--- tests/test_corpus.py
+++ tests/test_corpus.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 import pytest
 import sys
 sys.path.append('lib')
@@ -9,16 +10,41 @@ test_file = "tests/test.html"
 def pytest_funcarg__test_corp(request):
     return corpus.open(test_file)
 
+# ------- test ------------------------
 def test_init_corpus():
     c = corpus.EnMarkCorpus(test_file)
     assert c.vocab['<unk>'] == 0
 
+def test_size(test_corp):
+    assert 5 < test_corp.size()
+
 def test_parse(test_corp):
-    assert test_corp.rows[0] == ["<bol>", "<s>", "james", "</s>", "<v>", "is", "</v>", "a", "teacher", ".", "<eol>"]
-    assert test_corp.rows[1] == ["<bol>", "i", "am", "james", ".", "<eol>"]
-    assert test_corp.rows[2] == ["<bol>", "i", "have", "not", "<eol>"]
-    assert test_corp.rows[3] == ["<bol>", "he", "has", "not", "<eol>"]
-    assert test_corp.rows[4] == ["<bol>", "what", "is", "up", "<eol>"]
+    assert test_corp.get_row(0) == ["<bos>", "<s>", "james", "</s>", "<v>", "is", "</v>", "a", "teacher", ".", "<br>", "<eos>"]
+
+def test_parse_abbrev(test_corp):
+    assert test_corp.get_row(1) == ["<bos>", "i", "am", "james", ".", "<br>", "<eos>"]
+    assert test_corp.get_row(2) == ["<bos>", "i", "have", "not", "<br>", "<eos>"]
+    assert test_corp.get_row(3) == ["<bos>", "he", "has", "not", "<br>", "<eos>"]
+    assert test_corp.get_row(4) == ["<bos>", "what", "is", "up", "<br>", "<eos>"]
+
+def test_parse_number(test_corp):
+    assert test_corp.get_row(5) == ["<bos>", "<number>", "yen", ".", "<br>", "<eos>"]
 
 def test_vocab(test_corp):
     assert "james" in test_corp.vocab
+
+def test_add_vocab(test_corp):
+    num = len(test_corp.vocab)
+    test_corp.add_vocab('hello_world_this_is_dummy')
+    assert test_corp.vocab['hello_world_this_is_dummy'] == num
+
+def test_tokenize(test_corp):
+    assert test_corp.tokenize("<s>James</s> is.") == ["<bos>", "<s>", "james", "</s>", "is", ".", "<eos>"]
+    assert test_corp.tokenize("<s>James</s> is.", cleanup_tag=True) == ["<bos>", "james", "is", ".", "<eos>"]
+    assert test_corp.tokenize("3 > 1", cleanup_tag=True) == ["<bos>", "3", ">", "1", "<eos>"]
+
+def test_encode(test_corp):
+    assert [1, 7, 10, 14, 3] == test_corp.encode("james is.")
+
+def test_decode(test_corp):
+    assert "<bos> james is . <eos>" == test_corp.decode([1, 7, 10, 14, 3])
