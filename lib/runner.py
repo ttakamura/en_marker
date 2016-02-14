17aa1d970f21b0cbcc4b1a3e3d12fdf8144f5e38
Tatsuya Takamura <takamura@tonchidot.com>
2016-02-15 06:17:04 +0900

    [from now] Mon Feb 15 06:17:04 JST 2016

---
+++ lib/runner.py
@@ -0,0 +1,51 @@
+from __future__ import unicode_literals
+import numpy as np
+
+import chainer
+from chainer import Chain, Variable, cuda, optimizer, optimizers, serializers
+import chainer.functions as F
+import chainer.links as L
+
+import config
+import encdec
+
+def forward(src_batch, trg_batch, corpus, encdec, is_training, generation_limit):
+  batch_size = len(src_batch)
+  src_len = len(src_batch[0])
+  trg_len = len(trg_batch[0]) if trg_batch else 0
+  src_stoi = src_vocab.stoi
+  trg_stoi = trg_vocab.stoi
+  trg_itos = trg_vocab.itos
+  encdec.reset(batch_size)
+
+  x = XP.iarray([src_stoi('</s>') for _ in range(batch_size)])
+  encdec.encode(x)
+  for l in reversed(range(src_len)):
+    x = XP.iarray([src_stoi(src_batch[k][l]) for k in range(batch_size)])
+    encdec.encode(x)
+
+  t = XP.iarray([trg_stoi('<s>') for _ in range(batch_size)])
+  hyp_batch = [[] for _ in range(batch_size)]
+
+  if is_training:
+    loss = XP.fzeros(())
+    for l in range(trg_len):
+      y = encdec.decode(t)
+      t = XP.iarray([trg_stoi(trg_batch[k][l]) for k in range(batch_size)])
+      loss += functions.softmax_cross_entropy(y, t)
+      output = cuda.to_cpu(y.data.argmax(1))
+      for k in range(batch_size):
+        hyp_batch[k].append(trg_itos(output[k]))
+    return hyp_batch, loss
+
+  else:
+    while len(hyp_batch[0]) < generation_limit:
+      y = encdec.decode(t)
+      output = cuda.to_cpu(y.data.argmax(1))
+      t = XP.iarray(output)
+      for k in range(batch_size):
+        hyp_batch[k].append(trg_itos(output[k]))
+      if all(hyp_batch[k][-1] == '</s>' for k in range(batch_size)):
+        break
+
+    return hyp_batch
