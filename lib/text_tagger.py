e9976443cb3649885b71500f3815aaaa2a8c64f7
Tatsuya Takamura <takamura@tonchidot.com>
2016-03-21 07:58:42 +0900

    [from now] Mon Mar 21 07:58:42 JST 2016

--- lib/text_tagger.py
+++ lib/text_tagger.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 import codecs
 import numpy as np
 from nltk.tag.perceptron import PerceptronTagger
+import nltk
 
 class DummyPosTagger:
     def tag(self, tokens):
@@ -9,7 +10,16 @@ class DummyPosTagger:
 
 class NERTagger:
     def __init__(self):
-        self.tagger = PerceptronTagger()
+        self.pos_tagger = PerceptronTagger()
 
     def tag(self, tokens):
-        return self.tagger.tag(tokens)
+        tree = nltk.ne_chunk(self.pos_tagger.tag(tokens))
+        tagged_tokens = []
+        for t in tree:
+            if type(t) == nltk.tree.Tree:
+                label = t.label()
+                for token in t:
+                    tagged_tokens.append((token[0], label))
+            else:
+                tagged_tokens.append(t)
+        return tagged_tokens
