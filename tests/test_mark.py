8ca44e1ed7775060bc338d654cf21ba929393eb4
Tatsuya Takamura <takamura@tonchidot.com>
2016-03-24 07:05:32 +0900

    [from now] Thu Mar 24 07:05:32 JST 2016

--- tests/test_mark.py
+++ tests/test_mark.py
@@ -17,17 +17,18 @@ def test_idx_to_type():
 def test_convert_teach_id_row(test_corp):
     tokens   = ["<bos>", "<sj>", "james", "</sj>", "<v>", "is", "</v>", "<eos>"]
     mark_vec = mark.convert_teach_id_row(test_corp.tokens_to_ids(tokens), test_corp)
-    assert (mark_vec[0] == mark.convert_types_to_vec([])).all()
-    assert (mark_vec[1] == mark.convert_types_to_vec(['<sj>'])).all()
-    assert (mark_vec[2] == mark.convert_types_to_vec(['<v>'])).all()
-    assert (mark_vec[3] == mark.convert_types_to_vec([])).all()
+    assert mark_vec[0] == mark.convert_types_to_vec([])
+    assert mark_vec[1] == mark.convert_types_to_vec(['<sj>'])
+    assert mark_vec[2] == mark.convert_types_to_vec(['<v>'])
+    assert mark_vec[3] == mark.convert_types_to_vec([])
 
     tokens   = ["<bos>", "<sj>", "A", "<v>", "B", "</v>", "</sj>", "<eos>"]
     mark_vec = mark.convert_teach_id_row(test_corp.tokens_to_ids(tokens), test_corp)
-    assert (mark_vec[0] == mark.convert_types_to_vec([])).all()
-    assert (mark_vec[1] == mark.convert_types_to_vec(['<sj>'])).all()
-    assert (mark_vec[2] == mark.convert_types_to_vec(['<sj>', '<v>'])).all()
-    assert (mark_vec[3] == mark.convert_types_to_vec([])).all()
+    assert mark_vec[0] == mark.convert_types_to_vec([])
+    assert mark_vec[1] == mark.convert_types_to_vec(['<sj>'])
+    assert mark_vec[2] == mark.convert_types_to_vec(['<sj>', '<v>'])
+    assert mark_vec[3] == mark.convert_types_to_vec([])
+    #      (....).all()
 
 def test_convert_types_to_vec(test_corp):
     vec = mark.convert_types_to_vec(['<sj>', '<v>'])
